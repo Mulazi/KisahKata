01162ca61b5e926d8e76ba443082db6f89b4aacc
Farendza <farendza.muda@gmail.com>
2021-06-16 12:14:59 +0700

    nambah button di viewcontroller dan function

--- KisahKata/ViewController.swift
+++ KisahKata/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func button1(_ sender: Any) {
+    }
 }
 
